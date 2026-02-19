f3c0bf0bd9991ef3156f19c09161ecacb3e0b7e8
Bryan Morales <mor23283@uvg.edu.gt>
2026-02-18 22:48:23 -0600

    Update config.h

--- Proyecto1_Dig2/ADAFRUITconf----/ADAFRUITconf/config.h
+++ Proyecto1_Dig2/ADAFRUITconf----/ADAFRUITconf/config.h
@@ -2,11 +2,11 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME "EG3"
-#define IO_KEY "aio_sZpK5935BKjmw90cONIS26131k52"
+#define IO_USERNAME ""
+#define IO_KEY "aio_"
 
-#define WIFI_SSID "Edvin"
-#define WIFI_PASS "paiz moscoso"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 // comment out the following lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
